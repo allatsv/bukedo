6127203da576a1d89afdd9186d8609bfe7474ee5
allatsv <cvetkova20022@gmail.com>
2025-01-27 05:28:09 +0300

    cartJsx

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,11 +3,15 @@ import { Header } from './components';
 import { Footer } from './components';
 import { Recipient } from './components';
 import { MakeOrder } from './components';
+import { Counter } from './components';
+import { CountersList } from './components';
 import { Corporate,Bonussystem,Paymentdelivery, Agreement, AboutCompany, OrderPayment, Catalog } from './pages';
 function App() {
   return (
     <div className="App">
         <Header/>
+        <Counter/>
+        <CountersList/>
           <MakeOrder/>
           <Recipient/>
           <Catalog/>
--- my-app/src/components/index.js
+++ my-app/src/components/index.js
@@ -3,3 +3,5 @@ export { default as Footer } from './Footer';
 export { default as Card} from './Card'
 export {default as Recipient} from './Recipient'
 export {default as MakeOrder} from './MakeOrder'
+export {default as Counter} from './Counter'
+export {default as CountersList} from './CountersList'
